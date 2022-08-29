108293ccb674786106bc52a5ca1f412c4781da16
MicaelaConde <micaelaconde7@gmail.com>
2022-08-28 21:55:25 -0300

    Se realiza simulador interactivo

--- index.js
+++ index.js
@@ -8,13 +8,22 @@ while(i<=4){
         let alimentos =parseInt(prompt('1- Bolsa 1kg $120\n2- Bolsa 5kg $500\n3- Bolsa 10kg $840\n4- Salir')) 
         if (alimentos===1){
             compra=compra+120
+            break;
 
-        }if(alimentos===2){
+
+        }else if(alimentos===2){
             compra=compra+500
-        }if(alimentos===3){
+            break;
+        }else if(alimentos===3){
             compra=compra+840
+            break;
+        }
+        else if(alimentos===4){
+                i=5
+                break;
         }
         else{
+            alert('Ingrese una opcion valida')
             break;
         }
            
@@ -22,26 +31,35 @@ while(i<=4){
             let Juguetes =parseInt(prompt('1- Pelota $150\n2- Cuerda $80\n3- Hueso $130\n4- Salir')) 
             if (Juguetes===1){
                 compra=compra+150
+                break;
     
-            }if(Juguetes===2){
+            } else if(Juguetes===2){
                 compra=compra+80
-            }if(Juguetes===3){
+                break;
+            } else if(Juguetes===3){
                 compra=compra+130
-            }
-            else{
                 break;
             }
-            
+             else if(Juguetes===4){
+                i=5
+                break;
+        }
+            else{
+                alert('Ingrese una opcion valida')
+                break;
+            }
+
             case 3:
                 alert('Puedes contactarnos mediante\nWhatsapp: 095478452\nMail: petshop@gmail.com')
                 break;
             case 4:
+                i=5
                 break;
     }
 
-    i=menu
+    
 }
 
 
 
-alert(compra)
+alert("Total de su compra es  "+ "$"+compra)
